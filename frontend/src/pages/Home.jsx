4f3560f41c03556f24d7eecbecad99e00a0dc402
Darklove12 <bsinukela31@gmail.com>
2023-05-11 14:19:45 +0200

    added import view

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,3 +4,6 @@ import Ray from '../components/Ray'
 import Header from '../components/Header'
 import MultiCard from '../components/MultiCard'
 import MultiSell from '../components/MultiSell'
+import View from '../components/View'
+
+
