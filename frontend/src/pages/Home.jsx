11ef6c485725de8f3c97ea8ad863007c1f85ef70
Darklove12 <bsinukela31@gmail.com>
2023-05-11 14:12:01 +0200

    added import react for Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,4 @@
 import React from 'react'
-import Slider from '../components/Slider'
-import Ray from '../components/Ray'
-import Header from '../components/Header'
-import MultiCard from '../components/MultiCard'
-import MultiSell from '../components/MultiSell'
-import View from '../components/View'
 
-
-const Home = () => {
-  return (
-    <div>
-      <Header />
-      <Slider/>
-      <MultiCard/>
-      <MultiSell/>
-      <View/>
-      <Ray/>
-      
-    </div>
-  )
-}
 
 export default Home
