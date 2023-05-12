095cfb5e90aa88ad776d24489ba70ee63c457371
Darklove12 <bsinukela31@gmail.com>
2023-05-12 16:03:34 +0200

    created return to Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,21 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import './Header.css'
 
 function Header() {
-  
+  return (
+    <Navbar>
+      <Container>
+        <Navbar href="/"><span>Ray-Ban</span></Navbar>
+        <Navbar id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/contact">Contact Us</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/Profile">Profile</Nav.Link>
+          </Nav>
+        </Navbar>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
