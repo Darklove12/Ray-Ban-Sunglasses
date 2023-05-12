1f47c47cdaecf6030de69bcece947854ebdeb8fd
Darklove12 <bsinukela31@gmail.com>
2023-05-12 16:01:33 +0200

    created function and export

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,3 +5,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import './Header.css'
+
+function Header() {
+  
+}
+
+export default Header;
