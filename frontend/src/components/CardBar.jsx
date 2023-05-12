29aad36712d5e6309a1b6fab070edc009a4785e1
Darklove12 <bsinukela31@gmail.com>
2023-05-12 08:35:04 +0200

    added export

--- frontend/src/components/CardBar.jsx
+++ frontend/src/components/CardBar.jsx
@@ -2,4 +2,7 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import '../components/CardBar.css'
 
-function CardBar(props)
+function CardBar(props)  {
+}
+
+export default CardBar;
