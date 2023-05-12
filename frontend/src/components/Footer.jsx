f0c7786e80ecd08dc008b63bf405bae82d4fdd1c
Darklove12 <bsinukela31@gmail.com>
2023-05-12 14:45:02 +0200

    added import Foot fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,5 @@
 import React from 'react'
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Footer.css'
+
