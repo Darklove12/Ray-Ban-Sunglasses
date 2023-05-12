0eb494779e1e8120388a48522691b96489bfbf88
Darklove12 <bsinukela31@gmail.com>
2023-05-12 08:36:04 +0200

    created return

--- frontend/src/components/CardBar.jsx
+++ frontend/src/components/CardBar.jsx
@@ -3,6 +3,14 @@ import Card from 'react-bootstrap/Card';
 import '../components/CardBar.css'
 
 function CardBar(props)  {
+  return (
+    <card className="cardz">
+      <Card.Img src= {props.image} />
+      <card>
+        <card>{props.name}</card>
+      </card>
+    </card>
+  );
 }
 
 export default CardBar;
