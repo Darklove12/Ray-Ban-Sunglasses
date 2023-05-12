0d78d47da388891811a81bbe61748d591d45e421
Darklove12 <bsinukela31@gmail.com>
2023-05-12 15:58:53 +0200

    added mdb react

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,3 +3,4 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
