f23f3e58c250bc8e993ede11f48091294d5a5fb1
Darklove12 <bsinukela31@gmail.com>
2023-05-17 14:32:10 +0200

    added import React

--- frontend/src/components/SellCard.jsx
+++ frontend/src/components/SellCard.jsx
@@ -1,17 +1,1 @@
 import React from 'react'
-import './Sell.css'
-
-function Sell(props) {
-  return (
-    <>
-      <div class="card-image" >
-        <img src={props.image} className="cards-image" alt="..." />
-      </div>
-    </>
-  );
-}
-
-
-
-
-export default Sell;
