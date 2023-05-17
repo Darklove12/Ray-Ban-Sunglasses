03f8a33457a7f664dfb90aa5e5823022ec91f5f9
Darklove12 <bsinukela31@gmail.com>
2023-05-17 13:32:32 +0200

    created return and export

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,4 +7,22 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './Header.css'
 
-function Header() 
+function Header() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar href="/"><span>Ray-Ban</span></Navbar>
+        <Navbar id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/contact">Contact Us</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/Profile">Profile</Nav.Link>
+          </Nav>
+        </Navbar>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
