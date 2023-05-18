32f5381560aa563729ba3659200d835fc174ea3a
Darklove12 <bsinukela31@gmail.com>
2023-05-18 08:08:15 +0200

    added import Link

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,3 +2,4 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Header from '../components/Header'
+import { Link } from "react-router-dom";
